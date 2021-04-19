db710777b73e8a4f7eecf9d19be60e8f3ffc89bc
Benjamin Tikki Olsen <benjamin.tikki@gmail.com>
2021-04-19 11:00:01 +0200

    Final_v02

---
+++ CipherEncrypter/CipherEncrypter/CipherEncrypter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherEncrypter
+{
+    static class CipherEncrypter
+    {
+        public static string Encrypt(string inputString)
+        {
+            string encryptedOutputString = string.Empty;
+            byte[] charArray = Encoding.ASCII.GetBytes(inputString);
+
+            foreach (byte asciiValue in charArray)
+            {
+                int asciiIncremented = asciiValue + 1;
+                encryptedOutputString += asciiIncremented + "-";
+            }
+            encryptedOutputString = encryptedOutputString.Substring(0, encryptedOutputString.Length - 1);
+            return encryptedOutputString;
+        }
+
+        public static string Decrypt(string encryptedString)
+        {
+            string decryptedOutputString = string.Empty;
+            string[] encryptedArray = encryptedString.Split('-');
+
+
+            foreach (string asciiValueIncremented in encryptedArray)
+            {
+                int asciiValue = int.Parse(asciiValueIncremented) - 1;
+                char c = Convert.ToChar(asciiValue);
+
+                decryptedOutputString += c;
+            }
+
+            return decryptedOutputString;
+        }
+    }
+}
---
+++ CipherEncrypter/CipherEncrypter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherEncrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+            string inputString = Console.ReadLine();
+            string encryptedString = CipherEncrypter.Encrypt(inputString);
+
+            Console.WriteLine();
+            Console.Write("Encrypted string: ");
+            Console.WriteLine(encryptedString);
+
+            Console.WriteLine();
+            Console.Write("Decrypted string: ");
+            string decryptedString = CipherEncrypter.Decrypt(encryptedString);
+            Console.WriteLine(decryptedString);
+
+            Console.ReadKey();
+        }
+    }
+}
