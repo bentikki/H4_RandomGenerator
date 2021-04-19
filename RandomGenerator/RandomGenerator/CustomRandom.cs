1cb26f30aa8cdc88491e6ae7753d75b466dd15fe
Benjamin Tikki Olsen <benjamin.tikki@gmail.com>
2021-04-19 10:30:09 +0200

    Final

---
+++ RandomGenerator/RandomGenerator/CustomRandom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGenerator
+{
+    static class CustomRandom
+    {
+        public static byte[] GenerateRandomByteArray(int byteLength)
+        {
+            using (var randomGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[byteLength];
+                randomGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+    }
+}
--- RandomGenerator/RandomGenerator/Program.cs
+++ RandomGenerator/RandomGenerator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace RandomGenerator
 {
@@ -10,6 +11,33 @@ namespace RandomGenerator
     {
         static void Main(string[] args)
         {
+            int seed = 32;
+            Random rnd = new Random(seed);
+            Stopwatch stopwatch = new Stopwatch();
+            
+            Console.WriteLine("Random.Next:");
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(rnd.Next());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Stopwatch time: " + stopwatch.Elapsed);
+            Console.WriteLine();
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            Console.WriteLine("RNGCryptoServiceProvider:");
+            for (int i = 0; i < 10; i++)
+            {
+                int val = BitConverter.ToInt32(CustomRandom.GenerateRandomByteArray(seed), 0);
+                Console.WriteLine(val);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Stopwatch time: " + stopwatch.Elapsed);
+
+            Console.ReadKey();
+
         }
     }
 }
